6b41ac883b4cbf12650a553a3dc67e5bd7b213ec
Tai <lequoctai304@gmail.com>
2019-10-31 10:00:14 +0700

    test

---
+++ session1/test.php
@@ -0,0 +1,15 @@
+<!-- <?php
+	echo "Hello php";
+	echo "<br>";
+?> -->
+<?php
+	$userName = 'Tài';
+	echo $userName;
+	echo "<br>";
+		$userQQ = 'Đà Nẵng';
+	echo $userQQ;
+	echo "<br>";
+	$userPhone = '0784437590';
+	echo $userPhone;
+	echo "<br>";
+?>
